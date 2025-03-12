c4b3e060bea72220b5c3a9ae0b8a1e44e99d126f
Piyush Baburaj <piyushbaburj98@gmail.com>
2025-03-12 22:30:31 +0530

    Update CSS: Center submit button and adjust responsive layout for records

--- src/Components/Records/Records.js
+++ src/Components/Records/Records.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { Data } from "../../Context/WorkoutContext";
 import { useAuthContext } from "../../Hooks/useAuthContext";
 import "./Records.css";
@@ -14,29 +13,32 @@ const Records = () => {
       getWorkouts();
     }
   }, [user, getWorkouts]);
+
   return (
     <div className="records">
       {workouts &&
         workouts.map((item) => {
           return (
             <div className="record" key={item._id}>
-              <h1>Name of Exercise: {item.title}</h1>
-              <p>Reps: {item.reps}</p>
-              <p>Load(in kg): {item.load}</p>
+              <div className="content">
+                <h1>Name of Exercise: {item.title}</h1>
+                <p>Reps: {item.reps}</p>
+                <p>Load (in kg): {item.load}</p>
+              </div>
               <div className="btns">
                 <img
                   onClick={() => {
                     toggleUpdate(item);
                   }}
                   src={require("../../assets/edit.png")}
-                  alt=""
+                  alt="Edit"
                 />
                 <img
                   onClick={() => {
                     deleteWorkout(item._id);
                   }}
                   src={require("../../assets/delete.png")}
-                  alt=""
+                  alt="Delete"
                 />
               </div>
             </div>
